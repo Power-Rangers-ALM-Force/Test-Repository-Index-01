0032d760288ff4c4ce92f52f82621af4b97cd4d8
Utku Görkem Ertürk <utgoerweb@gmail.com>
2020-05-11 22:09:23 +0300

    test

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -16,10 +16,13 @@ public class PersonTest {
         assertEquals(2, person.getAge());
     }
 
+    /*
     @Test
     public void getHeight() {
         Person person = new Person("Ali",2,3);
         assertEquals(3, person.getHeight());
     }
+     */
+
 
 }
